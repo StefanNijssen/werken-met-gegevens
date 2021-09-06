f8be47c1407138e1d1891842c0333717af296f40
StefanNijssen <58977715+StefanNijssen@users.noreply.github.com>
2021-09-06 11:51:46 +0200

    commit 2

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,5 +1,5 @@
 def Feestlunch():
     kosten = 17 * 0.39 + 2 * 2.78
     korting = 3 * 0.5
-    print("Totale kosten = " + str(kosten - korting))
+    print("De feestlunch kost je bij de bakker " + str(kosten - korting) + " euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!")
 Feestlunch()
