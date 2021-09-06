77fc746aab1e96879b771c61d9050aa1ae3601f2
StefanNijssen <58977715+StefanNijssen@users.noreply.github.com>
2021-09-06 11:38:59 +0200

    .

---
+++ Untitled-1.py
@@ -0,0 +1,10 @@
+def Feestlunch():
+    kosten = 17 * 0.39 + 2 * 2.78
+    korting = 3 * 0.5
+    print("Totale kosten = " + str(kosten - korting))
+Feestlunch()
+def Speeldag():
+    kosten = 7.45 * 3
+    VIP = 0.37 * 9 * 3
+    print("Totale kosten = " + str(kosten + VIP))
+Speeldag()
